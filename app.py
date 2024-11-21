4a7b8c75834f51a78baca3797d1174db93a3ea93
Marco Cheung <marco1992@gmail.com>
2024-11-21 18:08:10 +0800

    Refactor file uploader logic in app.py for improved clarity and simplify response generation prompt

--- app.py
+++ app.py
@@ -37,49 +37,30 @@ def generate_response(prompt, image1_info, image2_info=None):
 col1, col2 = st.columns(2)
 
 with col1:
-    uploaded_file1 = st.file_uploader("Shop Invoice", key="file1")
+    uploaded_file = st.file_uploader("Shop Invoice", key="file")
 
 # Display uploaded image
-if uploaded_file1 is not None:
-    image1 = PIL.Image.open(uploaded_file1)
-    image1_info = Image.from_bytes(uploaded_file1.getvalue())
+if uploaded_file is not None:
+    image = PIL.Image.open(uploaded_file)
+    image_info = Image.from_bytes(uploaded_file.getvalue())
 
-    #Display col2 file uploader if uploaded_file1 is not None
-    with col2:
-        uploaded_file2 = st.file_uploader("Shop Invoice (2nd image when necessary)", key="file2")
-
-    # Create a container to display images side by side
-    image_container = st.container()
-
-    with image_container:
-        col_img1, col_img2 = st.columns(2)
-        
-        with col_img1:
-            st.image(image1, caption='Uploaded Image 1.', use_container_width=True)
-        
-        with col_img2:
-            if uploaded_file2 is not None:
-                image2 = PIL.Image.open(uploaded_file2)
-                image2_info = Image.from_bytes(uploaded_file2.getvalue())
-                st.image(image2, caption='Uploaded Image 2.', use_container_width=True)          
-            else:
-                image2_info = None
-    
+    st.image(image, caption='Uploaded Image.', use_container_width=True)
+   
     # Create the generative model
     generative_multimodal_model = GenerativeModel("gemini-1.5-flash-002")
 
     # Generate contents
-    prompt = """Return shop name, order date (null if not present on the receipt) and final payment amount only.
+    prompt = """Return shop name, order date and final payment amount only.
     1) Error check:
-       If shop name detected in the second image differs from the first, return null JSON value for "shop_name", "order_date", "payment_total". 
-       For "remarks" in JSON output, return error message "Detected disparate shop name, please re-upload two invoice images for a single transaction only."
-       Otherwise, follow the instructions below.
+       - If shop name and/or final payment amount is not detected, return null JSON value for "shop_name", "order_date", "payment_total". 
+         For "remarks" in JSON output, return error message "Please re-upload invoice images clearly."
+         Otherwise, follow the instructions below.
 
     2) - Output: Return solely the Markdown content without any additional explanations or comments.
        - No Delimiters: Do not use code fences or delimiters like ```markdown.
        - Complete Content: Do not omit any part of the page, including headers, footers, and subtext.
-       - Shop Name Format: Keep the first row of detected texts only.
-       - Order Date Format: Change to date format (YYYY-MM-DD) if detected.
+       - Shop Name Format: Keep the first row of detected texts only. Remove any additional texts.
+       - Order Date Format: Change to date format (YYYY-MM-DD) if detected. Otherwise, return null JSON value.
        - Final Payment Format: Do not include detected texts.
        - Remarks: Error message if any, else return null JSON value.
      """
@@ -108,13 +89,14 @@ if uploaded_file1 is not None:
     "required": ["shop_name", "order_date", "payment_total", "remarks"]
     }
 
-    response = generate_response(prompt, image1_info, image2_info)
+    response = generate_response(prompt, image_info)
 
     content = response.text.encode().decode('utf-8')
     st.write(content)
 
     # Display the result
-    # Parse the content as JSON and display it in a code block
-    #json_response = json.loads(content)
-    #pretty_json = json.dumps(json_response, indent=4)
-    #st.code(pretty_json, language='json')
+    with col2:
+        # Parse the content as JSON and display it in a code block
+        json_response = json.loads(content)
+        pretty_json = json.dumps(json_response, indent=4)
+        st.code(pretty_json, language='json')
