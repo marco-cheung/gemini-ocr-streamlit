540acf2308a6ac8d2c14addf6eb4d81ca7123569
Marco Cheung <marco1992@gmail.com>
2024-11-22 09:12:50 +0800

    Add support for uploading a second shop invoice image and update model version

--- app.py
+++ app.py
@@ -37,17 +37,36 @@ def generate_response(prompt, image1_info, image2_info=None):
 col1, col2 = st.columns(2)
 
 with col1:
-    uploaded_file = st.file_uploader("Shop Invoice")
+    uploaded_file1 = st.file_uploader("Shop Invoice", key="file1")
 
 # Display uploaded image
-if uploaded_file is not None:
-    image = PIL.Image.open(uploaded_file)
-    image_info = Image.from_bytes(uploaded_file.getvalue())
+if uploaded_file1 is not None:
+    image1 = PIL.Image.open(uploaded_file1)
+    image1_info = Image.from_bytes(uploaded_file1.getvalue())
 
-    st.image(image, caption='Uploaded Image.', use_container_width=True)
+    #Display col2 file uploader if uploaded_file1 is not None
+    with col2:
+        uploaded_file2 = st.file_uploader("Shop Invoice (2nd image when necessary)", key="file2")
+
+    # Create a container to display images side by side
+    image_container = st.container()
+
+    with image_container:
+        col_img1, col_img2 = st.columns(2)
+        
+        with col_img1:
+            st.image(image1, caption='Uploaded Image 1.', use_container_width=True)
+        
+        with col_img2:
+            if uploaded_file2 is not None:
+                image2 = PIL.Image.open(uploaded_file2)
+                image2_info = Image.from_bytes(uploaded_file2.getvalue())
+                st.image(image2, caption='Uploaded Image 2.', use_container_width=True)          
+            else:
+                image2_info = None
    
     # Create the generative model
-    generative_multimodal_model = GenerativeModel("gemini-1.5-flash")
+    generative_multimodal_model = GenerativeModel("gemini-1.5-flash-002")
 
     # Generate contents
     prompt = """
@@ -100,14 +119,12 @@ if uploaded_file is not None:
     "required": ["shop_name", "order_date", "order_datetime", "invoice_num", "payment_total", "remarks"]
     }
 
-    response = generate_response(prompt, image_info)
+    response = generate_response(prompt, image1_info, image2_info)
 
     content = response.text.encode().decode('utf-8')
     #st.write(content)
 
-    # Display the result
-    with col2:
-        # Parse the content as JSON and display it in a code block
-        json_response = json.loads(content)
-        pretty_json = json.dumps(json_response, indent=4)
-        st.code(pretty_json, language='json')
+    # Parse the content as JSON and display it in a code block
+    json_response = json.loads(content)
+    pretty_json = json.dumps(json_response, indent=4)
+    st.code(pretty_json, language='json')
