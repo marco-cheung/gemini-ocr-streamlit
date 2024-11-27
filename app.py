62b6b0c8ae44443a81326dc8b29d65d3d5eea831
Marco Cheung <marco1992@gmail.com>
2024-11-27 14:27:02 +0000

    Update tuned model endpoint to new endpoint ID for improved performance

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ with col3:
         
             # Create the generative model
             #generative_multimodal_model = GenerativeModel("gemini-1.5-pro-002") # "gemini-1.5-flash-002" for faster response
-            tuned_model_endpoint_name = 'projects/1081365314029/locations/us-central1/endpoints/2270105582776090624' # "gemini-pro-exp002"
+            tuned_model_endpoint_name = 'projects/1081365314029/locations/us-central1/endpoints/2000851506009473024' # "gemini-pro-exp003"
             tuned_model = GenerativeModel(tuned_model_endpoint_name)
 
             # Generate contents
